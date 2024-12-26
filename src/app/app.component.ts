d3814965bceedab0a8148af69c75cad3f6a74eff
Leandro Gonçalves <159172886+lleandrogon@users.noreply.github.com>
2024-12-25 21:55:11 -0300

    Criando a tela de entrada e iniciando o componente header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SplashComponent } from "./components/splash/splash.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SplashComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Altitude-Grafica';
+export class AppComponent implements OnInit {
+  showSplash: boolean = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showSplash = false;  
+    }, 0)
+  }
 }
