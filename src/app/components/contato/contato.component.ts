441ade388ea009cce88cbf2a595cb66c936451f2
Leandro Gonçalves <lleandrogon2004@gmail.com>
2024-12-29 20:30:00 -0300

    Adicionando componente de contato

---
+++ src/app/components/contato/contato.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contato',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './contato.component.html',
+  styleUrl: './contato.component.css'
+})
+export class ContatoComponent {
+  contatoForm: FormGroup = new FormGroup({
+    nome: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    telefone: new FormControl(null, [Validators.required, Validators.pattern(/^(\(\d{2}\) )?\d{4,5}-?\d{4}$/),]),
+    mensagem: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+  })
+
+  formatarTelefone(event: any): void {
+    const input = event.target as HTMLInputElement;
+    let valor = input.value.replace(/\D/g, '');
+
+    if (valor.length > 10) {
+      valor = valor.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+    } else if (valor.length > 6) {
+      valor = valor.replace(/^(\d{2})(\d{4})(\d{0,4})$/, '($1) $2-$3');
+    } else if (valor.length > 2) {
+      valor = valor.replace(/^(\d{2})(\d{0,5})$/, '($1) $2');
+    } else {
+      valor = valor.replace(/^(\d*)$/, '($1');
+    }
+
+    input.value = valor;
+    this.contatoForm.controls['telefone'].setValue(valor, { emitEvent: false });
+  }
+
+  limparFormulario() {
+    this.contatoForm.reset();
+  }
+
+  enviarWhatsapp() {
+    const nome = this.contatoForm.value.nome;
+    const email = this.contatoForm.value.email;
+    const telefone = this.contatoForm.value.telefone;
+    const mensagem = this.contatoForm.value.mensagem;
+
+    const numero = "5582988103004";
+    const mensagemWhatsapp = `Nome: ${nome}\nEmail: ${email}\n Telefone: ${telefone}\n\nMensagem: ${mensagem}`;
+    const codificacao = encodeURIComponent(mensagemWhatsapp);
+    const link = `https://wa.me/${numero}?text=${codificacao}`;
+
+    window.open(link, '_blank');
+  }
+}
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from "../home/home.component";
 import { SobreComponent } from "../sobre/sobre.component";
 import { ServicosComponent } from "../servicos/servicos.component";
 import { EstatisticasComponent } from "../estatisticas/estatisticas.component";
+import { ContatoComponent } from "../contato/contato.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent, SobreComponent, ServicosComponent, EstatisticasComponent],
+  imports: [HeaderComponent, HomeComponent, SobreComponent, ServicosComponent, EstatisticasComponent, ContatoComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
